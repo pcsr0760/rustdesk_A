76a19094bb50378d45ebb93549c44b57fb3dc524
RustDesk Automation <rustdesk@example.com>
2026-06-23 01:38:08 -0400

    Update configuration

--- src/common.rs
+++ src/common.rs
@@ -1081,7 +1081,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rd.570011.xyz:21114".to_owned()
 }
 
 #[inline]
